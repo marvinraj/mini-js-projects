7e33148aecc5450ab01d7cb86cb994cf3bf26831
Marvin <marvinraj77@gmail.com>
2024-04-16 08:37:06 +0800

    created a separate fucntion to flip the colour

--- flip colours/flip.js
+++ flip colours/flip.js
@@ -2,13 +2,18 @@
 //     document.getElementById("flip").style.backgroundColor="cyan";
 // }
 
-// get the colors, button and color
+// store list of colours and button from html file
 const colours = ["#AFC8AD", "#F0DBAF", "#DC8686", "#B06161", "#FFC6AC", "#C4C1A4"]
 const button = document.getElementById("flip-button")
 
 // event listener
-button.addEventListener("click", function() {
+button.addEventListener("click", flipColour)
+
+// function for event listener
+function flipColour() {
     // random num
     let randNum = Math.round(Math.random() * 5)
+
+    // change colour of body
     document.body.style.backgroundColor = colours[randNum]
-})
+}
