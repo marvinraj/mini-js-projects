1ce18a803d8034f667b12e7dbafbbfe8135e4a66
Marvin <marvinraj77@gmail.com>
2024-04-28 12:45:02 +0800

    completed day

--- cool online clock/index.js
+++ cool online clock/index.js
@@ -1,19 +1,24 @@
 const hour = document.getElementById("hour")
 const minute = document.getElementById("minute")
 const second = document.getElementById("second")
-
+const day = document.getElementById("day")
 
 function updateClock(){
     const now = new Date();
     const hour_now = now.getHours()
     const minute_now = now.getMinutes()
     const second_now = now.getSeconds()
-    const day_now = now.getDay()
-    const date_now = now.getDate()
+    var day_now = now.getDay()
+    
+
+    let days_array = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    
 
     hour.innerHTML = hour_now
     minute.innerHTML = minute_now
     second.innerHTML = second_now
+    day.innerHTML = days_array[day_now]
+    
 }
 
 setInterval(updateClock, 1000)
